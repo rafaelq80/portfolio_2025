2b3556584feef830b8c695ed5dc54061c5757ff6
Rafael Queiroz <rafaelproinfo@gmail.com>
2025-11-03 06:02:05 -0300

    :bug: Correção do HTML

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -33,7 +33,7 @@ async function getApiGithub() {
             <figure class="about_image">
                  <img src="${perfil.avatar_url}" alt="Foto do perfil do Github - ${perfil.name}" />
             </figure>
-            <div class="about_content">
+            <article class="about_content">
                 <h2>Sobre mim</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, consectetur sint corporis voluptate nam laboriosam nostrum nihil, dicta voluptas minus consequatur? Nesciunt soluta similique voluptate repellat perferendis sit quod accusantium.</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus veniam ab ad at quod? Dolorem recusandae voluptates tenetur laboriosam cupiditate, libero ab ipsum, soluta veritatis beatae odit nostrum vel! Explicabo.</p>
@@ -50,7 +50,7 @@ async function getApiGithub() {
 						</div>
 					</div>
                 </div>
-            </div>
+            </article>
     `
 		// PASSO 4: Adiciona o HTML criado dentro da seção "sobre"
 		// innerHTML += adiciona o conteúdo sem apagar o que já existe
